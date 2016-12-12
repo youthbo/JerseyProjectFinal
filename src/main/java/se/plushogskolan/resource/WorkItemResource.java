8b7e3712b614978b59e0ad5796dd8b523a84a41e
danielptm <danielptm@me.com>
2016-12-12 13:09:42 +0100

    Added WorkItemResource

---
+++ src/main/java/se/plushogskolan/resource/WorkItemResource.java
@@ -0,0 +1,33 @@
+package se.plushogskolan.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import se.plushogskolan.service.WorkItemService;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
+
+/**
+ * Created by daniel on 12/12/16.
+ */
+@Component
+@Path("workitem")
+@Produces(MediaType.APPLICATION_JSON)
+public final class WorkItemResource {
+
+    @Autowired
+    private WorkItemService workItemService;
+
+    @Context
+    private UriInfo uriInfo;
+
+    @Context
+    private HttpHeaders httpHeaders;
+
+
+
+}
