778ba7606b38f98ae9e04733d1e1e9f47bc68915
Mathias Hultman <hultman@mensa.se>
2016-12-12 15:00:31 +0100

    added initial files

--- src/main/java/se/plushogskolan/resource/WorkItemResource.java
+++ src/main/java/se/plushogskolan/resource/WorkItemResource.java
@@ -2,13 +2,19 @@ package se.plushogskolan.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import se.plushogskolan.model.WorkItem;
 import se.plushogskolan.service.WorkItemService;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
 /**
@@ -17,6 +23,7 @@ import javax.ws.rs.core.UriInfo;
 @Component
 @Path("workitems")
 @Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public final class WorkItemResource {
 
     @Autowired
@@ -28,6 +35,10 @@ public final class WorkItemResource {
     @Context
     private HttpHeaders httpHeaders;
 
-
+    @GET
+    public Response create(){
+    	System.out.println("check");
+    	return Response.ok().build();
+    }
 
 }
