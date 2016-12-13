75f85ccc34a53981ddec74fe92973842ba5e4596
danielptm <danielptm@me.com>
2016-12-13 14:25:53 +0100

    JerseyConfig changes

--- src/main/java/se/plushogskolan/config/JerseyConfig.java
+++ src/main/java/se/plushogskolan/config/JerseyConfig.java
@@ -4,10 +4,13 @@ import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 
 import se.plushogskolan.resource.UserResource;
+import se.plushogskolan.resource.WorkItemResource;
 
 @Component
 public class JerseyConfig extends ResourceConfig{
         public JerseyConfig() {
+
 			register(UserResource.class);
+			register(WorkItemResource.class);
 		}
 }
